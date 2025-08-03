6dc1217e77163bf78f24d1ec7cb31619d3bdc379
Igor Machado <souzalift@gmail.com>
2025-08-02 23:19:11 -0300

    fix: correct database name in MongoDB connection

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,6 +4,6 @@ export const connectToDatabase = async () => {
   if (mongoose.connections[0].readyState) return;
 
   await mongoose.connect(process.env.MONGODB_URI!, {
-    dbName: 'ovestiario',
+    dbName: 'vestiario',
   });
 };
