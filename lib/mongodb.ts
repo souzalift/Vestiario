2c985ecf731a22fdb8a6266ac0942fa7c7b878b3
Igor Machado <souzalift@gmail.com>
2025-08-16 21:24:11 -0300

    refactor: remove admin authentication and MongoDB connection code

--- lib/mongodb.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-export const connectToDatabase = async () => {
-  if (mongoose.connections[0].readyState) return;
-
-  await mongoose.connect(process.env.MONGODB_URI!, {
-    dbName: 'vestiario',
-  });
-};
