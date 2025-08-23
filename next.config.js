b8f1be73fa09b4729f804731a6ae704470e3c63c
Igor Machado <souzalift@gmail.com>
2025-08-23 13:20:20 -0300

    feat: add rewrites for Firebase Hosting integration

--- next.config.js
+++ next.config.js
@@ -6,4 +6,14 @@ const nextConfig = {
   images: { unoptimized: true },
 };
 
+// Rewrites for Firebase Hosting integration
+rewrites: () => ({
+  fallback: [
+    {
+      source: '/__/:path*',
+      destination: 'https://o-vestiario-67951.firebaseapp.com/__/:path*',
+    },
+  ],
+});
+
 module.exports = nextConfig;
